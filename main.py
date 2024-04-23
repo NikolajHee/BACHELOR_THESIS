ab4cc1188552128aa8a2f0719e8fbc1c8fdc1555
Nikolaj Hertz <nikolajhertz@Nikolajs-MacBook.local>
2024-04-23 16:23:16 +0200

    bayesian sweep

--- main.py
+++ main.py
@@ -44,17 +44,17 @@ parser.add_argument('--model_path', default=None, nargs='+')
 
 
 # variables that can be sweeped
-parser.add_argument('-c', '--classifier', default=[None], choices=['logistic', 'svc'], nargs='+')
-parser.add_argument('-hd', '--hidden_dim', default=[64], type=int, nargs='+')
-parser.add_argument('-od', '--output_dim', default=[320], type=int, nargs='+')
-parser.add_argument('-bs', '--batch_size', default=[8], type=int, nargs='+')
-parser.add_argument('-ne', '--n_epochs', default=[200], type=int, nargs='+')
-parser.add_argument('-lr', '--learning_rate', default=[0.001], type=float, nargs='+')
-parser.add_argument('-p', default=[0.5], type=float, nargs='+')
-parser.add_argument('-gc', '--grad_clip', default=[None], type=float, nargs='+')
-parser.add_argument('--N_train', default=[None], type=int, nargs='+')
-parser.add_argument('--N_test', default=[None], type=int, nargs='+')
-parser.add_argument('--alpha', default=[0.5], type=float, nargs='+')
+parser.add_argument('-c', '--classifier', default=None, choices=['logistic', 'svc'], nargs='+')
+parser.add_argument('-hd', '--hidden_dim', default=64, type=int, nargs='+')
+parser.add_argument('-od', '--output_dim', default=320, type=int, nargs='+')
+parser.add_argument('-bs', '--batch_size', default=8, type=int, nargs='+')
+parser.add_argument('-ne', '--n_epochs', default=200, type=int, nargs='+')
+parser.add_argument('-lr', '--learning_rate', default=0.001, type=float, nargs='+')
+parser.add_argument('-p', default=0.5, type=float, nargs='+')
+parser.add_argument('-gc', '--grad_clip', default=None, type=float, nargs='+')
+parser.add_argument('--N_train', default=None, type=int, nargs='+')
+parser.add_argument('--N_test', default=None, type=int, nargs='+')
+parser.add_argument('--alpha', default=0.5, type=float, nargs='+')
 
 # manual features 
 #   not implemented
@@ -92,8 +92,25 @@ def main(sweep=True):
 
     config = wandb.config
 
+    test = vars(config)
+
+    test2 = {i:j for (i,j) in test['_items'].items() if i!='_wandb'}
+
+    #print(arguments)
+    #print(test2)
+    for (i,j) in test2.items():
+        arguments[i] = j
+    
+    # remove lists
+    for (i,j) in arguments.items():
+        if type(j) == list:
+            arguments[i] = j[0]
+
+    #print(arguments)
+
+    #raise NotImplementedError
     # save path
-    save_path = os.path.join(results_path, config.dataset, dt_string)
+    save_path = os.path.join(results_path, arguments['dataset'], dt_string)
 
     # initialize time object
     time_object = TimeTaking(save_path=save_path)
@@ -106,45 +123,45 @@ def main(sweep=True):
     save_parameters(save_path, vars(args))
 
     # set random seed
-    if config.seed is not None:
-        random_seed(config.seed)
+    if arguments['seed'] is not None:
+        random_seed(arguments['seed'])
 
 
     # load data
-    if config.dataset == 'PTB_XL':
+    if arguments['dataset'] == 'PTB_XL':
         from dataset import PTB_XL
         dataset = PTB_XL()
     else:
         from dataset import AEON_DATA
         # UCR and UEA datasets
-        dataset = AEON_DATA(config.dataset)
+        dataset = AEON_DATA(arguments['dataset'])
 
-    print(f"using the data {config.dataset}")
+    print(f"using the data {arguments['dataset']}")
 
     # manual features
     #  not implemented
-    if config.manual_features:
-        from manual_features import model
-        return
+    # if config.manual_features:
+    #     from manual_features import model
+    #     return
 
 
     # create train/test-split
     from utils import train_test_dataset
     train_dataset, test_dataset = train_test_dataset(dataset=dataset,
                                                      test_proportion=0.3,
-                                                     train_size=config.N_train,
-                                                     test_size=config.N_test,
-                                                     return_stand=config.normalize)
+                                                     train_size=arguments['N_train'],
+                                                     test_size=arguments['N_test'],
+                                                     return_stand=arguments['normalize'])
                                                     
     # Either train a model or load existing model
-    if not args.model_path:
+    if not arguments['model_path']:
         from TS2VEC import TS2VEC
 
         # initialize model
-        model = TS2VEC(input_dim=config.input_dim,
-                       hidden_dim=config.hidden_dim,
-                       output_dim=config.output_dim,
-                       p=config.p,
+        model = TS2VEC(input_dim=arguments['input_dim'],
+                       hidden_dim=arguments['hidden_dim'],
+                       output_dim=arguments['output_dim'],
+                       p=arguments['p'],
                        device=DEVICE)
 
         # start time object
@@ -153,15 +170,16 @@ def main(sweep=True):
         # train the framework
         model.train(train_dataset=train_dataset,
                     test_dataset=test_dataset,
-                    n_epochs=config.n_epochs,
-                    batch_size=config.batch_size,
-                    learning_rate=config.learning_rate,
-                    grad_clip=config.grad_clip,
-                    alpha=config.alpha,
+                    n_epochs=arguments['n_epochs'],
+                    batch_size=arguments['batch_size'],
+                    learning_rate=arguments['learning_rate'],
+                    grad_clip=arguments['grad_clip'],
+                    alpha=arguments['alpha'],
                     wandb=wandb,
                     train_path=save_path,
-                    t_sne=config.t_sne,
-                    classifier=config.classifier,)
+                    t_sne=arguments['t_sne'],
+                    classifier=arguments['classifier'],
+                    )
 
         # end time object
         time_object.end('Model Training')
@@ -173,14 +191,14 @@ def main(sweep=True):
 
             train_dataset, test_dataset = train_test_dataset(dataset=dataset,
                                                      test_proportion=0.3,
-                                                     train_size=config.N_train,
-                                                     test_size=config.N_test,
-                                                     return_stand=config.normalize)
+                                                     train_size=arguments['N_train'],
+                                                     test_size=arguments['N_test'],
+                                                     return_stand=arguments['normalize'])
 
             from torch.utils.data import DataLoader
 
-            train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, drop_last=True)
-            test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True, drop_last=True)
+            train_dataloader = DataLoader(train_dataset, batch_size=arguments['batch_size'], shuffle=True, drop_last=True)
+            test_dataloader = DataLoader(test_dataset, batch_size=arguments['batch_size'], shuffle=True, drop_last=True)
 
             from cda import cca
             cca(model=model.model,
@@ -194,17 +212,17 @@ def main(sweep=True):
         from TS2VEC import Encoder
 
 
-        encoder = Encoder(input_dim=config.input_dim,
-                               hidden_dim=config.hidden_dim,
-                               output_dim=config.output_dim,
-                               p=config.p).to(DEVICE)
+        encoder = Encoder(input_dim=arguments['input_dim'],
+                               hidden_dim=arguments['hidden_dim'],
+                               output_dim=arguments['output_dim'],
+                               p=arguments['p']).to(DEVICE)
         
         model = torch.optim.swa_utils.AveragedModel(encoder)
 
         # insert path to model_save
-        t = args.model_path
+        t = arguments['model_path']
         
-        PATH = f"results/{config.dataset}/({t[1]}_{t[2]}_{t[3]})_({t[4]}_{t[5]}_{t[6]})"
+        PATH = f"results/{arguments['dataset']}/({t[1]}_{t[2]}_{t[3]})_({t[4]}_{t[5]}_{t[6]})"
         
 
         model.load_state_dict(torch.load(os.path.join(PATH, 'best_model.pt')))
@@ -212,19 +230,32 @@ def main(sweep=True):
 
 
 
+bayes = True
+
 # execute main function (sweep or single value training)
 if any([len(i) > 1 for i in arguments.values() if type(i) == list]):
     print("initializing sweep")
 
-    # create sweep configuration (grid search)
+    # # create sweep configuration (grid search)
+    # sweep_configuration = {
+    #     "method": "grid",
+    #     "metric": {"name": "tsloss/hierarchical_contrastive_loss", "goal": "minimize"},
+    #     "parameters": {
+    #         i : {"values": j if type(j) == list else [j]}
+    #         for (i,j) in arguments.items() 
+    #     },
+    # }
+
     sweep_configuration = {
-        "method": "grid",
-        "metric": {"name": "tsloss/test_loss", "goal": "minimize"},
+        "method": "bayes",
+        "name": "sweep_bayes_TS2VEC",
+        "metric": {"name": "tsloss/hierarchical_contrastive_loss", "goal": "minimize"},
         "parameters": {
-            i : {"values": j if type(j) == list else [j]}
-            for (i,j) in arguments.items() 
+            i : {"min": j[0], "max": j[1]} for (i,j) in arguments.items() if (type(j) == list) and (len(j) == 2)
         },
     }
+
+    #print(sweep_configuration['parameters'])
 
     sweep_id = wandb.sweep(sweep=sweep_configuration, project="BACHELOR_THESIS")
 
